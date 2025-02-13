37abb5ec32468a3713656b60d3b1f88372bed5f4
sruthadev <sruthadev@gmail.com>
2025-02-13 23:46:40 +0530

    Handle click event added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import ListGroup from "./components/ListGroup";
 import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
 function App() {
   let items = ["Kochi", "Melbourne", "London", "New York"];
-    
+    const handleClick = (event: MouseEvent) => console.log(event)
   return (
     <>
       <h1>List Group</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((items) => (
-            <li key={items}>{items}</li>
+          <li
+            className="list-group-item"
+            key={items}
+            onClick={handleClick}
+          >
+            {items}
+          </li>
         ))}
       </ul>
     </>
